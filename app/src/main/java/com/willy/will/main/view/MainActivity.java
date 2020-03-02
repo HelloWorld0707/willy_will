224c8d7cf0e43d0e0e99ec3b1ff7a86d5a1f34c6
Jaennie <je028770@gmail.com>
2020-03-02 14:56:23 +0900

    Update remaker

--- app/src/main/java/com/willy/will/main/view/MainActivity.java
+++ app/src/main/java/com/willy/will/main/view/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends AppCompatActivity{
 
 
     MainFragment fragmentmain;
-    CalendarActivity fragmentcalender;
 
 
     @Override
@@ -62,18 +61,12 @@ public class MainActivity extends AppCompatActivity{
         DBAccess dbHelper = new DBAccess(this, "test.db", null, 1);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        /*
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        */
-
-        //set navigation Drawer
+        /**set navigation Drawer**/
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerView = (View) findViewById(R.id.nav_view);
-        //~set navigation Drawer
+        /*~set navigation Drawer*/
 
-        //setDate
-
+        /**setDate**/
         tv_date = (TextView) findViewById(R.id.tv_date);
         todayDate = Calendar.getInstance();
         sdf = new SimpleDateFormat("MM.dd");
@@ -81,9 +74,9 @@ public class MainActivity extends AppCompatActivity{
         baseDate = sdf.format(todayDate.getTime());
         sendDate = sdf2.format(todayDate.getTime());
         tv_date.setText(baseDate);
-        //~setDate
+        /*~setDate*/
 
-        //open picker & change txt
+        /**open picker & change txt**/
         tv_date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -91,8 +84,11 @@ public class MainActivity extends AppCompatActivity{
                         todayDate.get(Calendar.YEAR), todayDate.get(Calendar.MONTH), todayDate.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+        /*~open picker & change txt*/
 
-        //set sp_group (fix later)
+
+        /**set sp_group **/
+        //(fix later)
         spgroupList = new ArrayList<>();
         for(int i=0; i<5;i++){
             spgroupList.add("그룹"+i);
@@ -116,17 +112,17 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onNothingSelected(AdapterView<?> parent) {}
         });
-        //~Set sp_group
+        /*Set sp_group*/
 
-        //set fragment (don't change position)
+        /**set fragment (don't change position)**/
         fragmentmain = MainFragment.getInstance(sendDate);
 
         //add the fragment to container(frame layout)
         getSupportFragmentManager()
                 .beginTransaction().add(R.id.fragmentcontainer,fragmentmain).commit();
-        //~set fragment
+        /*~set fragment*/
 
-        // set fab event Listener
+        /** set fab event Listener **/
         FloatingActionButton fab = findViewById(R.id.fabItemAdd);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -136,16 +132,10 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
-        // ~set fab event Listener
+        /* ~set fab event Listener */
     }
 
-    /**
-     * Last Modified: -
-     * Last Modified By: -
-     * Created: 2020-02-19
-     * Created By: Lee Jaeeun
-     * Function: Setting action for drawer menu
-     */
+    /**Setting action for drawer menu**/
     DrawerLayout.DrawerListener naviListener = new DrawerLayout.DrawerListener() {
         @Override
         public void onDrawerSlide(@NonNull View drawerView, float slideOffset) { }
@@ -161,69 +151,37 @@ public class MainActivity extends AppCompatActivity{
         @Override
         public void onDrawerStateChanged(int newState) { }
     };
+    /*~Setting action for drawer menu*/
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }*/
 
-    /**
-     * Last Modified: 20-02-29
-     * Last Modified By: Lee Jaeeun
-     * Created: -
-     * Created By: -
-     * Function: Move to SearchView
-     * @param view
-     */
+    /** Function: Move to SearchView */
     public void btnSearchClick(View view){
         Intent intent = new Intent(MainActivity.this , SearchActivity.class);
         intent.putExtra(getResources().getString(R.string.current_date_key),sendDate);
         Log.d("DateChecked","**********날짜"+sendDate+"*************");
         startActivity(intent);
     }
+    /*~ Function: Move to SearchView */
 
 
-    /**
-     * Last Modified: 20-02-24
-     * Last Modified By: Lee Jaeeun
-     * Created: -
-     * Created By: Lee Jaeeun
-     * Function: Move to CalendarView (temporary connect to fragment)
-     * @param view
-     */
+    /** Move to CalendarView */
     public void btnCalendarClick(View view) {
         Intent intent = new Intent(MainActivity.this , CalendarActivity.class);
         intent.putExtra(getResources().getString(R.string.current_date_key),sendDate);
         Log.d("DateChecked","**********날짜"+sendDate+"*************");
         startActivity(intent);
     }
+    /*~Move to CalendarView*/
 
-    /**
-     * Last Modified: -
-     * Last Modified By: -
-     * Created: 2020-02-19
-     * Created By: Lee Jaeeun
-     * Function: Open navigation drawer
-     * @param view
-     */
+    /** Open navigation drawer */
     public void btnSettingClick(View view){
         naviListener.onDrawerOpened(drawerView);
     }
+    /*~ Open navigation drawer */
 
 
-    /**
-     * Last Modified: 2020-02-26
-     * Last Modified By: Lee Jaeeun
-     * Created: 2020-02-24
-     * Created By: Lee Jaeeun
-     * Function: Open Date picker and deleting original fragment
-     *           and creating new fragment of selected date
-     * @param y (year)
-     * @param m (month)
-     * @param d (day)
-     */
+    /** Open Date picker and deleting original fragment
+        and creating new fragment of selected date **/
 
     DatePickerDialog.OnDateSetListener datepicker = new DatePickerDialog.OnDateSetListener() {
         @Override
@@ -247,39 +205,15 @@ public class MainActivity extends AppCompatActivity{
 
         }
     };
+    /*~ Open Date picker and deleting original fragment
+     and creating new fragment of selected date */
 
-    /**
-     * Last Modified: -
-     * Last Modified By: -
-     * Created: 2020-02-24
-     * Created By: Lee Jaeeun
-     * Function: Change Date Using from selected date by Date Picker
-     */
+    /** Change Date Using from selected date by Date Picker **/
     private void updateLabel(){
         baseDate = sdf.format(todayDate.getTime());
         sendDate = sdf2.format(todayDate.getTime());
         tv_date.setText(baseDate);
     }
+    /* ~ Change Date*/
 
-    /*
-    @Override
-    // tool bar
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        //
-        if (id == R.id.itemSearch){
-            // show search activity
-            Intent intent = new Intent(MainActivity.this , SearchActivity.class);
-            startActivity(intent);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 }
--- app/src/main/java/com/willy/will/main/view/MainFragment.java
+++ app/src/main/java/com/willy/will/main/view/MainFragment.java
@@ -19,12 +19,6 @@ public class MainFragment extends Fragment {
     // dont fix it
     private final static String EXTRA_ADAPTER = "BaseAdpater";
 
-    // example extra string
-    /*
-    private final static String EXTRA_INT = "someInt";
-    private final static String EXTRA_STRING = "someTitle";
-    */
-
     //Recycler View
     private RecyclerView recyclerView = null;
 
@@ -32,13 +26,7 @@ public class MainFragment extends Fragment {
     private static final String ARG_NO = "ARG_NO";
 
 
-    /**
-     * Last Modified: -
-     * Last Modified By: -
-     * Created: 2020-02-19
-     * Created By: Lee Jaeeun
-     * Function: setting fragment
-     */
+    /**setting fragment*/
     public static MainFragment getInstance(String dateString){
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
@@ -46,27 +34,10 @@ public class MainFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+    /*~setting fragment*/
 
-    /*
-    // omission database object
-    public static final MainFragment newInstance(int page, String title, BaseAdapter adapter) {
-        // example of communication
-        MainFragment fragment = new MainFragment();
-        Bundle args = new Bundle();
-        args.putInt(EXTRA_INT, page);
-        args.putString(EXTRA_STRING, title);
 
-        //
-        args.putSerializable(EXTRA_ADAPTER, (mainListAdapter)adapter);
-        //
-
-        fragment.setArguments(args);
-        return fragment;
-        // ~example
-    }
-     */
-
-    // Store instance variables based on arguments passed
+    /** Store instance variables based on arguments passed */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,14 +46,16 @@ public class MainFragment extends Fragment {
         String text = checkString + "날의 프래그먼트";
         Log.d("MyFragment", "onCreate " + text+"***************************************");
     }
+    /* ~Store instance variables based on arguments passed */
 
-    // Inflate the view for the fragment based on layout XML
+    /** Inflate the view for the fragment based on layout XML*/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         ViewGroup rootView =
                 (ViewGroup)inflater.inflate(R.layout.fragment_main,container,false);
 
+        /** Set TodoItem */
         ArrayList<ToDoItem> list = new ArrayList<>();
         ToDoItem sample = new ToDoItem();
         for(int i=0; i<20;i++){
@@ -90,58 +63,21 @@ public class MainFragment extends Fragment {
             sample.setTime(getArguments().getString(ARG_NO,"Today"));
             list.add(sample);
         }
+        /* ~Set TodoItem */
 
-        // Set RecyclerView
+        /** Initialization (including Item View)*/
         // ↓↓↓↓↓↓↓↓↓↓ RecyclerViewAdapter 매개변수 고치는 바람에 부득이하게 수정함
         recyclerView = new RecyclerViewSetter(
                 R.id.mainItemList, rootView,
                 R.integer.to_do_recycler_item_type, list,
                 R.string.selection_id_main, false
         ).setRecyclerView();
-        /*recyclerView = view.findViewById(R.id.mainItemList);
-        recyclerView.setHasFixedSize(true); //안정적...고정사이즈
-        // set LayoutManager
-        layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
-        // set Adapter
-        final int TYPE = getResources().getInteger(R.integer.to_do_recycler_item_type);
-        adapter = new RecyclerViewAdapter(TYPE, list);//itemlist
-        recyclerView.setAdapter(adapter);
-        // set Tracker
-        tracker = new SelectionTracker.Builder<>(
-                "example",
-                recyclerView,
-                new StableIdKeyProvider(recyclerView),
-                new RecyclerItemDetailsLookup(recyclerView),
-                StorageStrategy.createLongStorage()
-        ).withSelectionPredicate(
-                // Select multiple items
-                SelectionPredicates.<Long>createSelectSingleAnything()
-        ).build(); // 하나만 선택
-        adapter.setTracker(tracker);
-        tracker.addObserver(new SelectionTracker.SelectionObserver() {
-            */
-        /**
-             * Last Modified: 2020-02-12
-             * Last Modified By: Shin Minyong
-             * Created: -
-             * Created By: -
-             * Function: Initialization (including Item View)
-             * */
-
-            /*
-            @Override //선택에 변화가 있을 떄 사용함
-            public void onSelectionChanged() {
-                super.onSelectionChanged();
-                Intent intent = new Intent(getContext(), DetailActivity.class);
-                startActivity(intent);
-            }
-        });*/
-        // ↑↑↑↑↑↑↑↑↑↑ RecyclerViewAdapter 매개변수 고치는 바람에 부득이하게 수정함
+        /* ~Initialization (including Item View) */
 
         return rootView;
 
     }
+    /* ~Inflate the view for the fragment based on layout XML*/
 
     @Override
     public void onResume() {
