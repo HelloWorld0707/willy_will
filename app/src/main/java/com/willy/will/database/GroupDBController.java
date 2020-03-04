282830b2f8d3fdb995d7051e3ac2bb0648c1b53e
Shin <smy11go@gmail.com>
2020-03-04 18:19:56 +0900

    Read groups of database

--- app/src/main/java/com/willy/will/database/GroupDBController.java
+++ app/src/main/java/com/willy/will/database/GroupDBController.java
@@ -1,13 +1,31 @@
 package com.willy.will.database;
 
-import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-public class GroupDBController {
-    private DBAccess databaseAccess = null;
-    private SQLiteDatabase readDatabase = null;
+import com.willy.will.common.model.Group;
 
-    public GroupDBController(Context context) {
-        //databaseAccess = dbHelper
+import java.util.ArrayList;
+
+public class GroupDBController {
+    private SQLiteDatabase readDatabase = null;
+    private SQLiteDatabase writeDatabase = null;
+
+    public GroupDBController() {
+        readDatabase = DBAccess.getDbHelper().getReadableDatabase();
+        writeDatabase = DBAccess.getDbHelper().getWritableDatabase();
+    }
+
+    public ArrayList<Group> getAllGroups(ArrayList<Group> arrayList) {
+        Cursor cursor = readDatabase.query("_GROUP", null, null, null, null, null, "group_name ASC");
+        Group curGroup = null;
+        while(cursor.moveToNext()) {
+            curGroup = new Group();
+            curGroup.setGroupId(cursor.getInt(cursor.getColumnIndexOrThrow("group_id")));
+            curGroup.setGroupName(cursor.getString(cursor.getColumnIndexOrThrow("group_name")));
+            curGroup.setGroupColor(cursor.getString(cursor.getColumnIndexOrThrow("group_color")));
+            arrayList.add(curGroup);
+        }
+        return arrayList;
     }
 }
