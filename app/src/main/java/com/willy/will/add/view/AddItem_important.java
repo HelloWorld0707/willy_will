305a0305dc717e9c0ef6be905ccaf3e9e3bfeac6
SeongsikSeo <sungsik9831@gmail.com>
2020-03-10 21:32:15 +0900

    SEONGSIK

--- app/src/main/java/com/willy/will/add/view/AddItem_important.java
+++ app/src/main/java/com/willy/will/add/view/AddItem_important.java
@@ -1,71 +1,58 @@
-package com.willy.will.search.view;
+package com.willy.will.add.view;
 
-import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.willy.will.R;
 import com.willy.will.adapter.RecyclerViewAdapter;
 import com.willy.will.adapter.RecyclerViewSetter;
-import com.willy.will.common.model.PopupActivity;
 import com.willy.will.common.model.RecyclerViewItemType;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
-public class DoneSearchSettingActivity extends PopupActivity {
-
+public class AddItem_important extends AppCompatActivity {
     private String selectedDoneKey = null;
-
     private RecyclerView recyclerView = null;
-
-    private ArrayList<String> doneList = null;
-
-    // Initialization (including layout ID)
-    public DoneSearchSettingActivity() {
-        super(R.layout.activity_done_search_setting);
-    }
+    private ArrayList<String> importantList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        /** Set data of item **/
-        doneList = new ArrayList<>();
+        importantList = new ArrayList<>();
         Resources resources = getResources();
-        doneList.add(resources.getString(R.string.all));
-        doneList.add(resources.getString(R.string.not_done));
-        doneList.add(resources.getString(R.string.done));
-        /* ~Set data of item */
+        importantList.add(resources.getString(R.string.First));
+        importantList.add(resources.getString(R.string.Second));
+        importantList.add(resources.getString(R.string.Third));
+        importantList.add(resources.getString(R.string.empty));
 
-        /** Set Views **/
         recyclerView = new RecyclerViewSetter(
-                R.id.done_search_setting_recycler_view, getWindow().getDecorView(),
-                RecyclerViewItemType.DONE_SEARCH, doneList,
+                R.id.important_recycler_view, getWindow().getDecorView(),
+                RecyclerViewItemType.DONE_SEARCH, importantList,
                 R.string.selection_id_done_search_setting, false
         ).setRecyclerView();
-        /* ~Set Views */
 
         /** Set a selected item **/
         selectedDoneKey = resources.getString(R.string.selected_done_key);
+
         String selectedDone = getIntent().getStringExtra(selectedDoneKey);
-        for(int i = 0; i < doneList.size(); i++) {
-            if(doneList.get(i).equals(selectedDone)) {
+        for(int i = 0; i < importantList.size(); i++) {
+            if(importantList.get(i).equals(selectedDone)) {
                 long selectedPosition = Long.valueOf(i);
                 ((RecyclerViewAdapter) recyclerView.getAdapter()).getTracker().select(selectedPosition);
             }
         }
         /* ~Set a selected item */
     }
-
-    @Override
+    /**@Override
     protected boolean setResults(Intent intent) {
         Iterator selectIter = ((RecyclerViewAdapter) recyclerView.getAdapter()).getTracker().getSelection().iterator();
         int selectedIndex = Math.toIntExact((Long) selectIter.next());
-        intent.putExtra(selectedDoneKey, doneList.get(selectedIndex));
+        intent.putExtra(selectedDoneKey, importantList.get(selectedIndex));
         return true;
-    }
+    }**/
 
 }
